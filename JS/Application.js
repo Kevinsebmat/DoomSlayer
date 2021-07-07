611bc9c945380a7c0db2c8742c0d65e6dad90b85
Silvy Mathews <silvy.mathews@gmail.com>
2021-07-06 20:55:14 -0700

    mute button on off

--- JS/Application.js
+++ JS/Application.js
@@ -24,3 +24,22 @@ function launchApplication() {
     //     music.play()
     // })
 }
+
+
+function toggleSound() {
+    console.log('am here in toggle sound')
+    var audioElem = document.getElementById('my_audio');
+    if (audioElem.paused) {
+        audioElem.play();
+        $(".my-image").attr("src", "resources/images/mute.png");
+    } else {
+        audioElem.pause();
+        $(".my-image").attr("src", "resources/images/speaker.png");
+    }
+}
+
+// function toggleClass() {
+//     $(".plus").click(function() {
+//         $(this).toggleClass("minus");
+//     })
+// }
